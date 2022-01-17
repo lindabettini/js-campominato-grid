61e615bb243df5d5ec54476a7a65effb2b626138
lindabettini <linda.bettini@hotmail.it>
2022-01-17 15:55:29 +0100

    Add functions

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ todo - con difficoltà 2 => tra 1 e 81
 todo - con difficoltà 3 => tra 1 e 49
 todo - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
+
+// // FUNZIONI 
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getUniqueRandomNumber = (min, max, list) => {
+    let randNumber;
+    do {
+        randNumber = getRandomNumber(min, max);
+    }   while (list.includes(randNumber));
+
+    return randNumber;
+}
+ 
+
+// // VARIABILI BASILARI
+let cells = 5;
+let colums = 10; 
+const totalCells = cells * colums; // ? la sposto giù? 
+
