4dce76242acf57a1461c60f6cc01d8ec90796809
lindabettini <linda.bettini@hotmail.it>
2022-01-17 15:23:46 +0100

    first push

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+todo - L'utente indica TRAMITE DOM un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+todo - con difficoltà 1 => tra 1 e 100
+todo - con difficoltà 2 => tra 1 e 81
+todo - con difficoltà 3 => tra 1 e 49
+todo - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
