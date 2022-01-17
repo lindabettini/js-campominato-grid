3b2730c2f22c7570bbb58199d2d4761a25db2349
lindabettini <linda.bettini@hotmail.it>
2022-01-17 19:05:11 +0100

    Add grid

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 todo - L'utente indica TRAMITE DOM un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
 todo - con difficoltà 1 => tra 1 e 100
 todo - con difficoltà 2 => tra 1 e 81
-todo - con difficoltà 3 => tra 1 e 49
+todo - con difficoltà 3 => tra 1 e 49 
+todo -  (10 * 10 = 100) (9 * 9 = 81) (7 * 7 = 49) (100% / ${cells}
 todo - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
