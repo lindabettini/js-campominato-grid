4ae777c96dd4b8194b916aa6bb8f001e24fd7ac0
lindabettini <linda.bettini@hotmail.it>
2022-01-18 00:01:25 +0100

    Da fixare griglia

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ todo -  (10 * 10 = 100) (9 * 9 = 81) (7 * 7 = 49) (100% / ${cells}
 todo - Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
-// // FUNZIONI 
+// // FUNZIONI NEUTRE
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getUniqueRandomNumber = (min, max, list) => {
@@ -22,40 +22,65 @@ const getUniqueRandomNumber = (min, max, list) => {
 //ç______________________________________________________
 //ç______________________________________________________
 
-// // PRENDO ELEMENTI DA HTML
+// // PREDO ELEMENTI DA DOM
 
 const grid = document.getElementById('grid');
 const selectField = document.getElementById('select-level');
 
-// // CREO LE VARIABILI BASILARI
-
-let selectValue;
-let selectMessage;
-let totalCells; // ? la sposto giù? 
+// // AGGIUNGO EVENTO ALLA SELECT
 
 selectField.addEventListener('change', (event) => {
+
     // ° Creo variabile con valore della Select
-    selectValue = selectField.value;
+    const selectValue = selectField.value;
     console.log(selectValue);
 
     // ° Stampo scelta della Select
     const selectMessage = document.getElementById('choice-result'); 
     selectMessage.textContent = `You choose ${event.target.value}`;
 
-    // ° Attribuisco diverso numero celle ai diversi valori della Select
-    switch(selectValue){
-        case 'beginner':
-            totalCells = 100;
-            console.log(totalCells);
-            break;
-        case 'medium':
-            totalCells = 81;
-            console.log(totalCells);
-            break;
-        case 'expert':
-            totalCells = 49;
-            console.log(totalCells);
-            break;    
+    // ° Creo una funzione per calcolare il numero delle celle    
+    function calculateCellsNumber(cellsNumber, columnsNumber) {
+        const totalCellsNumber = cellsNumber * columnsNumber;
+        return totalCellsNumber;
     }
 
+     // ° Creo una funzione per aggiungere celle nel DOM 
+     function createCell() {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        return cell;
+    }
+    // ° Funzione per appendere celle al dom (con dentro funzione CreateCell)
+    function appendCell(domContainerOfCell, totalNumberOfCells) {
+        for (let i = 0; i < totalNumberOfCells; i++) {
+            const cell = createCell();
+            domContainerOfCell.appendChild(cell);
+            if (selectValue === 'beginner') {
+                cell.classList.add('small');
+            } else if (selectValue === 'medium') {
+                cell.classList.add('medium');
+            } else {
+                cell.classList.add('large');
+            }
+        } 
+    }
+
+    // ° Attribuisco diverso numero celle ai diversi valori della Select
+    let totalCells;
+    switch(selectValue) {
+        case 'beginner':
+            totalCells = calculateCellsNumber(10, 10);
+            appendCell(grid, totalCells);      
+            break;
+        case 'medium':
+            totalCells = calculateCellsNumber(9, 9);
+            appendCell(grid, totalCells); 
+            break;
+        case 'expert':
+            totalCells = calculateCellsNumber(7, 7);
+            appendCell(grid, totalCells);             
+            break;   
+    }
+        
   });
