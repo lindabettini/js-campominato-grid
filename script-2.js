5268ad5f3918211cb70727387e9653afc9fcd0f1
lindabettini <linda.bettini@hotmail.it>
2022-01-18 15:08:05 +0100

    Add new script

---
+++ script-2.js
@@ -0,0 +1,36 @@
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min +1)) + min;
+
+const play = () => {
+    // ° Cambio testo bottone
+    playButton.innerText = 'Ricomincia';
+
+    // ° Elimino eventuale griglia precedente 
+    const grid = document.getElementById('grid');
+    grid.innerHTML= '';
+
+    // // FASE PREPARATORIA 
+    let attempts = 0;
+    const TOTAL_BOMBS = 16; 
+
+    const level = document.getElementById('select-level').value;
+    console.log('level');
+    
+    let totalCells; 
+    let cellsPerRow;
+
+    switch (level) {
+        case 'beginner':
+            totalCells = 100;
+            break;
+        case 'expert':
+            totalCells = 49;
+            break;
+        default: 
+        totalCells = 81;
+    }
+    cellsPerRow = Math.sqrt(totalCells);
+
+    
+}
+
+const playButton = document.getElementById('play');
